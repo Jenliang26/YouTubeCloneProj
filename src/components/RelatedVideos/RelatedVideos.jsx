50a3602b17ca161d6a6187db2f23d43e7085e6bd
n-rdy <giuseppegelardi@icloud.com>
2021-08-23 15:17:45 -0500

    properly implemented the .map to return an array of iframes to display related videos

--- src/components/RelatedVideos/RelatedVideos.jsx
+++ src/components/RelatedVideos/RelatedVideos.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 
 const RelatedVideos = (props) => {
   // const [relatedVideos, setRelatedVideos] = useState([])
-
+  
   return ( 
     <div>
       <h1>Related Videos</h1>
       <button onClick={() => console.log(props.videos)}>give me the fuckin data!
-    </button>
+      </button>
       {/* props.videos.map(video) => {} */}
+      <br />
+      {props.videos.map((video) => {
+        return <iframe title="videoPlayer" id="ytplayer" type="text/html" width="160" height="90"
+        src={`https://www.youtube.com/embed/${video}?autoplay=0&origin=http://example.com`}
+        frameborder="0"></iframe>
+      })}
       
       
 
