efc49af5bf079a567a7b37c165c23c15ca43d8a1
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-08-24 14:19:06 -0500

    Started working on the comment section

--- src/components/CommentsSection/CommentsSection.jsx
+++ src/components/CommentsSection/CommentsSection.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from from 'axios';
 
-const CommentsSection = () => {
+const CommentsSection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      video_id: '',
+      comments: '',
+      like: '',
+      dislike: '',
+    }
+  }
+
   return ( 
     <h1>Comments Section</h1>
    );
