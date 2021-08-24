cb68e03e366832fe5504813115688ec8317d9282
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-08-24 14:20:32 -0500

    Added comment as a props in App to use in the comment section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      videoSearchResults:[]
+      videoSearchResults:[],
+      comments: [],
     }
   }
 
